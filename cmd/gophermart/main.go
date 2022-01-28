c9dc8b4ccb1d6a89281819d38e7fc75ead94c997
magmel <magmel.programmer@gmail.com>
2022-01-28 21:40:57 +0300

    base context?

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"github.com/alexflint/go-arg"
+	"golang.org/x/sync/errgroup"
+	"log"
+	"net"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type config struct {
+	BaseServiceAddress    string `arg:"-a,env:RUN_ADDRESS" default:"localhost:8080"`
+	DatabaseDSN           string `arg:"-d,env:DATABASE_URI"`
+	AccrualServiceAddress string `arg:"-r,env:ACCRUAL_SYSTEM_ADDRESS" default:"localhost:8081"`
+}
 
 func main() {
-	fmt.Println("hello, world")
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	cfg := config{}
+	arg.MustParse(&cfg)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGKILL, syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	server := http.Server{
+		Addr:        cfg.BaseServiceAddress,
+		IdleTimeout: time.Second,
+		BaseContext: func(listener net.Listener) context.Context {
+			return ctx
+		},
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		log.Println("starting loyal system service...")
+		return server.ListenAndServe()
+	})
+
+	eg.Go(func() error {
+		<-ctx.Done()
+
+		log.Println("stopping the service...")
+
+		err := server.Shutdown(ctx)
+		return err
+	})
+
+	log.Println(eg.Wait())
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/magmel48/go-musthave-diploma
 
 go 1.17
+
+require (
+	github.com/alexflint/go-arg v1.4.2
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+)
+
+require github.com/alexflint/go-scalar v1.0.0 // indirect
