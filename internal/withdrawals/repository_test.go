8ce96b870f8d01009eb093fdd25b1ec37e86f78f
magmel <magmel.programmer@gmail.com>
2022-02-22 00:05:23 +0300

    one more test

--- internal/withdrawals/repository_test.go
+++ internal/withdrawals/repository_test.go
@@ -3,10 +3,11 @@ package withdrawals
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"github.com/DATA-DOG/go-sqlmock"
+	"reflect"
 	"regexp"
 	"testing"
+	"time"
 )
 
 func TestPostgreSQLRepository_Create(t *testing.T) {
@@ -31,8 +32,6 @@ func TestPostgreSQLRepository_Create(t *testing.T) {
 	e.WillReturnError(nil)
 	e.WithArgs(userID, orderNumber, amount)
 
-	fmt.Println(e.String())
-
 	tests := []struct {
 		name    string
 		fields  fields
@@ -55,6 +54,62 @@ func TestPostgreSQLRepository_Create(t *testing.T) {
 
 			if err := repository.Create(tt.args.ctx, tt.args.userID, tt.args.orderNumber, tt.args.amount); (err != nil) != tt.wantErr {
 				t.Errorf("Create() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestPostgreSQLRepository_FindByUser(t *testing.T) {
+	type fields struct {
+		db *sql.DB
+	}
+	type args struct {
+		ctx    context.Context
+		userID int64
+	}
+
+	userID := int64(1)
+	orderNumber := "100"
+	sum := 1.0
+	processedAt := time.Now()
+
+	db, sqlMock, _ := sqlmock.New()
+	e := sqlMock.ExpectQuery(
+		regexp.QuoteMeta(`SELECT "order", "sum", "processed_at" FROM "withdrawals" WHERE "user_id" = $1 ORDER BY "processed_at" ASC`))
+	e.WillReturnRows(sqlmock.NewRows([]string{"order", "sum", "processed_at"}).AddRow(orderNumber, sum, processedAt))
+	e.WillReturnError(nil)
+	e.WithArgs(userID)
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []Withdrawal
+		wantErr bool
+	}{
+		{
+			name:    "should execute proper query",
+			fields:  fields{db: db},
+			args:    args{ctx: context.TODO(), userID: userID},
+			wantErr: false,
+			want:    []Withdrawal{{Order: orderNumber, Sum: sum, ProcessedAt: processedAt}},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			repository := &PostgreSQLRepository{
+				db: tt.fields.db,
+			}
+
+			got, err := repository.FindByUser(tt.args.ctx, tt.args.userID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FindByUser() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FindByUser() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
